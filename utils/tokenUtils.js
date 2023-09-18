1429c187c1dc513aca32aabb3c7228ae84da66bb
gopal-codes <gopalraut9840285859@gmail.com>
2023-09-18 16:56:08 +0545

    second commit

--- utils/tokenUtils.js
+++ utils/tokenUtils.js
@@ -1,5 +1,9 @@
 "use client";
+
 export const getAdmin = () => {
+  if (typeof window !== 'undefined') {
+    // Perform localStorage action
   let admin = localStorage.getItem("loginKey");
   return JSON.parse(admin);
+  }
 };
